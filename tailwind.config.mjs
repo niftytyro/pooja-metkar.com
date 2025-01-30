a30deb233dc7635a5b008cee8f1be6ffc2cf3657
Udasi Tharani <udasitharani@gmail.com>
2025-01-30 07:39:37 +0530

    add crafting-financial-journeys page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,17 +3,23 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      black: "#000",
+      black: "#000000",
       black10: "#001820",
-      black20: "#393939",
+      black20: "#232323",
+      black30: "#393939",
+      black60: "#525252",
       black120: "#CFD6DF",
       yellow40: "#ECB81C",
       yellow100: "#FFFBF0",
       gray100: "#F0EFEC",
+      almondMilk: "#F6F3EC",
+      almondMilk20: "#FBFAF8",
+      saffron: "#F4CCCC",
     },
     extend: {
       fontFamily: {
         sans: ["Manrope", "sans-serif"],
+        magic: ["Are You Serious"],
       },
     },
   },
