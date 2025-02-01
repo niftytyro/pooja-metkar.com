96d4df06c9098d6060605303240ee5158b41f1ae
Udasi Tharani <udasitharani@gmail.com>
2025-02-01 11:39:24 +0530

    add loans research page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
+      white: "#FFFFFF",
       black: "#000000",
       black10: "#001820",
       black20: "#232323",
@@ -12,6 +13,7 @@ export default {
       yellow40: "#ECB81C",
       yellow100: "#FFFBF0",
       gray100: "#F0EFEC",
+      looksRed: "#E06513",
       almondMilk: "#F6F3EC",
       almondMilk20: "#FBFAF8",
       saffron: "#F4CCCC",
@@ -20,6 +22,10 @@ export default {
       fontFamily: {
         sans: ["Manrope", "sans-serif"],
         magic: ["Are You Serious"],
+      },
+      maxWidth: {
+        "desktop-content": 896,
+        "desktop-content-wide": 1048,
       },
     },
   },
